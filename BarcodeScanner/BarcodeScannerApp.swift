c1502b8ebfbaa61a1a393f02a0d9c49a1c3426f5
Jonathan Tri <jonathantric@gmail.com>
2023-01-24 11:38:23 +0700

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by JonathanTriC on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
